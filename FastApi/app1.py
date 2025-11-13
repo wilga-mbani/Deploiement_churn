60d8247669c4552c907ec977cf2b5d9d684c4c39
wilga-mbani <rovillon.mbani@gmail.com>
2025-11-13 14:45:11 +0100

    chargement des modification app1

--- FastApi/app1.py
+++ FastApi/app1.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 
 # --- URL de ton API FastAPI ---
-API_URL = "https://projet0-hjt9.onrender.com/predict"
+API_URL = "https://deploiement-churn.onrender.com/predict"
 
 # --- Configuration de la page ---
 st.set_page_config(page_title="Prédiction Churn", layout="wide")
